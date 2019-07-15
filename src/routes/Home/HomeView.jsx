65d19127362a98865776229b4cd5ce4df68bf4d5
andrea-bellini <a.bellini@peoplelab.it>
2019-07-15 12:16:36 +0200

    add example home route

---
+++ src/routes/Home/HomeView.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+
+const HomeComponent = () => (
+  <div>
+    Hello world!
+  </div>
+);
+
+
+HomeComponent.propTypes = {
+};
+
+HomeComponent.defaultProps = {
+};
+
+
+export default HomeComponent;
---
+++ src/routes/Home/index.js
@@ -0,0 +1,4 @@
+import { hot } from 'react-hot-loader/root';
+import Home from './HomeView';
+
+export default hot(Home);
